a2c4759f43d78204a59b31730a9359351fa68212
mouseliu <475973481@qq.com>
2014-11-05 17:18:29 +0800

    Signed-off-by: mouseliu <475973481@qq.com>

--- js/fwslider.js
+++ js/fwslider.js
@@ -1,11 +1,11 @@
-seajs.use(['arale/switchable/1.0.2/slide'], function(Slide) {
+seajs.use(['slide'], function(Slide) {
     var slide = new Slide({
         element: '#slide-demo-1',
         effect: 'fade',
         activeIndex: 0
     }).render();
 });
-seajs.use(['arale/switchable/1.0.2/slide'], function(Slide) {
+seajs.use(['slide'], function(Slide) {
     var slide = new Slide({
         element: '#slide-demo-2',
         effect: 'fade',
