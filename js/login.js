5b8285529d80fff210e8ac8f07d4ad72e8a26dd5
mouseliu <475973481@qq.com>
2014-11-06 20:08:49 +0800

    Signed-off-by: mouseliu <475973481@qq.com>

--- js/login.js
+++ js/login.js
@@ -20,7 +20,69 @@ $(function() {
     });
 });
 
+seajs.use(['validator', '$'], function(Validator, $) {
+    $(function() {
+        var NewValidator = Validator.extend({
+            attrs: {
+                showMessage: function (message, element) {
+                    message = '<i class="ui-tiptext-icon iconfont">&#xF045;</i>\
+                               <span class="ui-form-explain-text">' + message + '</span>';
+                    this.getExplain(element)
+                        .addClass('ui-tiptext ui-tiptext-error')
+                        .html(message);
+                    this.getItem(element).addClass(this.get('itemErrorClass'));
+                }
+            }
+        });
 
+        var validator = new NewValidator({
+            element: '#test-form',
+            onFormValidated: function(err, results, form) {
+                window.console && console.log && console.log(err, results, form);
+            },
+            failSilently: true
+        });
+
+        validator.addItem({
+            element: '[name="telphone"]',
+            required: true,
+            rule: 'mobile '
+        })
+
+        validator.addItem({
+            element: '[name=agree]',
+            required: true,
+            rule: 'checkbox ',
+            errormessageRequired: '请接受协议'
+        });
+
+        var validatorPass = new NewValidator({
+            element: '#test-form-pass',
+            onFormValidated: function(err, results, form) {
+                window.console && console.log && console.log(err, results, form);
+            },
+            failSilently: true
+        });
+         validatorPass.addItem({
+            element: '#username',
+            required: true,
+            rule: 'text'
+        })
+
+        validatorPass.addItem({
+            element: '#password',
+            required: true,
+            rule: 'minlength{"min":5} maxlength{"max":20}'
+        })
+
+        validatorPass.addItem({
+            element: '#password-confirmation',
+            required: true,
+            rule: 'confirmation{target: "#password", name: "第一遍"}',
+            errormessageRequired: '请再重复输入一遍密码，不能留空。'
+        })
+    });
+});
 
 
 
