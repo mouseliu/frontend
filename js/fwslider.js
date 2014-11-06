fcd11f7d18cc629c11eca6f1f1c2470282a17b2e
mouseliu <475973481@qq.com>
2014-11-06 16:13:57 +0800

    Signed-off-by: mouseliu <475973481@qq.com>  筛选 排序 css 结构

--- js/fwslider.js
+++ js/fwslider.js
@@ -2,6 +2,7 @@ seajs.use(['slide'], function(Slide) {
     var slide = new Slide({
         element: '#slide-demo-1',
         effect: 'fade',
+        autoplay:"true",
         activeIndex: 0
     }).render();
 });
@@ -9,6 +10,7 @@ seajs.use(['slide'], function(Slide) {
     var slide = new Slide({
         element: '#slide-demo-2',
         effect: 'fade',
+        autoplay:"true",
         activeIndex: 0
     }).render();
 });
