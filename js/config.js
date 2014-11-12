c864815b9f6fe5777d0697f2ffc2d37a06a58cdb
唐友华 <tangyouhua@iot.chinamobile.com>
2014-11-12 12:02:30 +0800

    增加dialog.css，调整注册验证细节

--- js/config.js
+++ js/config.js
@@ -35,3 +35,79 @@ define('hack$', [], function() {
 seajs.use('hack$');
 seajs.cache[seajs.resolve('$')] = seajs.cache[seajs.resolve('$-debug')] =
     seajs.cache[seajs.resolve('jquery')] = seajs.cache[seajs.resolve('jquery-debug')] = seajs.cache[seajs.resolve('hack$')];
+
+$.ajaxSetup({
+    cache: false,
+    dataType: 'json',
+    headers: {
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+    },
+    // @tofishes, add unok status tip
+    statusCode: {
+        500: function() {
+            sysTip.show('服务器错误(500), 请稍后重试! ');
+        },
+        503: function() {
+            sysTip.show('服务器繁忙(503)，请刷新重试！');
+        },
+        504: function() {
+            sysTip.show('请求超时(504)，请刷新重试！');
+        },
+        403: function() {
+            sysTip.show('无权限(403), 请尝试重新授权！');
+        }
+    }
+});
+
+/* 防止ajax重复请求过滤器 */
+// @tofishes
+$.ajaxPrefilter(function(options, originalOptions, jqXHR) {
+    var args = originalOptions;
+    var _url = args.url || location.href;
+    var data = args.data || {};
+    //参数名+url做唯一判定
+    var xhrId = [_url];
+    for (var key in data) {
+        xhrId.push(key);
+    }
+    xhrId = xhrId.join('');
+
+    var cacheXhr = window[xhrId];
+    if (cacheXhr && cacheXhr.readyState != 4) {
+        cacheXhr.abort();
+    }
+    window[xhrId] = jqXHR;
+
+    function reportErr(status) {
+        if (document.location.host != 'www.kmsocial.cn') {
+            return;
+        }
+        $.get('/kk.php', {
+            f: options.url,
+            requestStatus: status
+        });
+    }
+
+    options.success = function() {
+        if (!arguments[0] && options.url.indexOf('/kk.php') == -1) {
+            reportErr('closed');
+            if (/\/userAccShow/.test(options.url)) {
+                document.location.assign('/p/logout/');
+            }
+            return;
+        }
+        // @tofishes 暂时对 -99 需要登录做个处理
+        // 从isOK方法中移出到这里，防止被customErr过滤掉
+        var data = arguments[0];
+        if (data && data.errno === -99) {
+            window.location.assign('/p/logout/');
+            return false;
+        };
+        if (!options.customErr) { //如果自定义错误提示为true，跳过默认拦截
+            if (!t.isOK.apply(t, arguments)) {
+                return;
+            }
+        }
+        originalOptions.success && originalOptions.success.apply(options.context || this, arguments);
+    };
+});
--- js/reg.js
+++ js/reg.js
@@ -1,108 +1,106 @@
 // Login Form
 seajs.use(['validator', '$', 'md5'], function(Validator, $, md5) {
-        $(function() {
-                var NewValidator = Validator.extend({
-                        attrs: {
-                            itemSuccessClass: 'ui-form-item-success',
-                            showMessage: function(message, element) {
-                                message = '<i class="ui-tiptext-icon iconfont">&#xF045;</i>\
-                               <span class="ui-form-explain-text">' + message + '</span>';
-                                // message = '<i class="ui-tiptext-icon iconfont">&#xF049;</i>\
-                                //            <span class="ui-form-explain-text">' + message + '</span>';
-                                this.getExplain(element).html(element.attr('data-explain') || ' ');
-                                this.getExplain(element)
-                                    .addClass('ui-tiptext ui-tiptext-error')
-                                    .html(message);
-                                this.getItem(element).addClass(this.get('itemErrorClass'));
-                            },
-                            hideMessage: function(message, element) {
-                                message = "手机号码可用"
-                                message = '<i class="ui-tiptext-icon iconfont">&#xF049;</i>\
-                               <span class="ui-form-explain-text">' + message + '</span>';
-                                this.getExplain(element).html(element.attr('data-explain') || ' ');
-                                this.getExplain(element).removeClass('ui-tiptext-error')
-                                    .addClass('ui-tiptext ui-tiptext-success')
-                                    .html(message);
-                                this.getItem(element).addClass(this.get('itemSuccessClass'));
-                            }
-                        });
+    $(function() {
+        var NewValidator = Validator.extend({
+            attrs: {
+                itemSuccessClass: 'ui-form-item-success',
+                showMessage: function(message, element) {
+                    message = '<i class="ui-tiptext-icon iconfont">&#xF045;</i><span class="ui-form-explain-text">' + message + '</span>';
+                    // this.getExplain(element).html(element.attr('data-explain') || ' ');
+                    this.getExplain(element)
+                        .addClass('ui-tiptext ui-tiptext-error')
+                        .html(message);
+                    this.getItem(element).removeClass(this.get('itemSuccessClass')).addClass(this.get('itemErrorClass'));
+                },
+                hideMessage: function(message, element) {
+                    message = '<i class="ui-tiptext-icon iconfont">&#xF049;</i>';
 
-                    //reg step1
-                    var validator = new NewValidator({
-                        element: '#test-form',
-                        onFormValidated: function(err, results, form) {
-                            window.console && console.log && console.log(err, results, form);
-                        },
-                        failSilently: true
-                    });
-
-
-                    validator.addItem({
-                        element: '[name="telphone"]',
-                        required: true,
-                        rule: 'mobile ',
-                        errormessageRequired: '手机号码错误或已注册'
-                    }) validator.query('#test-form [name=telphone]').on('itemValidated', function(error, message, elem) {
-                        if (error == null) {
-                            var url = "http://127.0.0.1:81/json/login.json";
-                            $.ajax({
-                                url: url,
-                                //context: this,
-                                dataType: 'json',
-                                success: function(data) {
-                                    var has = false;
-                                    for (i = 0; i < data.length; i++) {
-                                        if (data[i].mobile === $("#telphone").val()) {
-                                            has = true;
-                                        }
-                                    }
-                                    if (has) {
-                                        message = "手机号码已注册"
-                                        console.log("手机号码已注册");
-                                    } else {
-                                        message = "手机号码可用"
-                                        console.log("手机号码可用");
-                                    };
-                                },
-                                error: function(message) {
-                                    //message
-                                }
-                            });
-                        };
-                    }); validator.addItem({
-                        element: '[name=agree]',
-                        required: true,
-                        rule: 'checkbox ',
-                        errormessageRequired: '请接受协议'
-                    });
-
-
-
-                    //reg step2
-                    var validatorPass = new NewValidator({
-                        element: '#test-form-pass',
-                        onFormValidated: function(err, results, form) {
-                            window.console && console.log && console.log(err, results, form);
-                            var reg_data = {
-                                mobile: '',
-                                password: md5($('#password').val() + 'iotmaill@2014', 'youhua'),
-                                validCode: ''
-                            }
-                        },
-                        failSilently: true
-                    })
-
-                    validatorPass.addItem({
-                        element: '#password',
-                        required: true,
-                        rule: 'minlength{"min":5} maxlength{"max":20}'
-                    })
-
-                    validatorPass.addItem({
-                        element: '#password-confirmation',
-                        required: true,
-                        rule: 'confirmation{target: "#password", name: "第一遍"}',
-                        errormessageRequired: '请再重复输入一遍密码，不能留空。'
-                    })
-                });
+                    this.getExplain(element).removeClass('ui-tiptext-error')
+                        .addClass('ui-tiptext ui-tiptext-success')
+                        .html(message);
+                    this.getItem(element).removeClass(this.get('itemErrorClass')).addClass(this.get('itemSuccessClass'));
+                }
+            }
         });
+
+        //reg step1
+        var validator = new NewValidator({
+            element: '#test-form',
+            onFormValidated: function(err, results, form) {
+                window.console && console.log && console.log(err, results, form);
+            },
+            failSilently: true
+        });
+
+
+        validator.addItem({
+            element: '[name="telphone"]',
+            required: true,
+            rule: 'mobile ',
+            errormessageRequired: '手机号码错误或已注册'
+        });
+        validator.query('#test-form [name=telphone]').on('itemValidated', function(error, message, elem) {
+            if (error == null) {
+                var url = "http://127.0.0.1:81/json/login.json";
+                $.ajax({
+                    url: url,
+                    //context: this,
+                    dataType: 'json',
+                    success: function(data) {
+                        var has = false;
+                        for (i = 0; i < data.length; i++) {
+                            if (data[i].mobile === $("#telphone").val()) {
+                                has = true;
+                            }
+                        }
+                        if (has) {
+                            message = "手机号码已注册"
+                            console.log("手机号码已注册");
+                        } else {
+                            message = "手机号码可用"
+                            console.log("手机号码可用");
+                        };
+                    },
+                    error: function(message) {
+                        //message
+                    }
+                });
+            };
+        });
+        validator.addItem({
+            element: '[name=agree]',
+            required: true,
+            rule: 'checkbox ',
+            errormessageRequired: '请接受协议'
+        });
+
+
+
+        //reg step2
+        var validatorPass = new NewValidator({
+            element: '#test-form-pass',
+            onFormValidated: function(err, results, form) {
+                window.console && console.log && console.log(err, results, form);
+                var reg_data = {
+                    mobile: '',
+                    password: md5($('#password').val() + 'iotmaill@2014', 'youhua'),
+                    validCode: ''
+                }
+            },
+            failSilently: true
+        })
+
+        validatorPass.addItem({
+            element: '#password',
+            required: true,
+            rule: 'minlength{"min":5} maxlength{"max":20}'
+        })
+
+        validatorPass.addItem({
+            element: '#password-confirmation',
+            required: true,
+            rule: 'confirmation{target: "#password", name: "第一遍"}',
+            errormessageRequired: '请再重复输入一遍密码，不能留空。'
+        })
+    });
+});
